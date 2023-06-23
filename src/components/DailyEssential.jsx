158af021086e98d3071c82736b7f71b884bdcdfa
abrar115 <abrar.alrahman@gmail.com>
2023-06-23 14:57:16 +0300

    first commit

--- src/components/DailyEssential.jsx
+++ src/components/DailyEssential.jsx
@@ -3,18 +3,29 @@ import { DailyEssentials } from "./lacalDatabase";
 
 function DailyEssential() {
   return (
-    <div className="w-4/5 m-auto grid sm:grid-cols-1 md:grid-cols-6 gap-4 py-8">
-      {DailyEssentials.map((x) => (
-        <div className="bg-gray-100 p-4 rounded-[15px] text-center" key={x._id}>
-          <img
-            className="w-[130px] h-[130px] rounded-[15px]"
-            src={x.image}
-            alt="dailEssential"
-          />
-          <h2 className="text-gray-500">{x.title}</h2>
-          <h1 className="font-bold text-[14px]">UP TO 50% OFF</h1>
-        </div>
-      ))}
+    <div className="">
+      <div className="w-4/5 m-auto">
+        <h1 className="border-b-[2px] pb-4 border-gray-100">
+          Daily Essentials
+        </h1>
+        <div className="border-b-2 border-[#05ABF3] w-[150px]" />
+      </div>
+      <div className="w-4/5 m-auto grid sm:grid-cols-1 md:grid-cols-6 gap-2 py-8">
+        {DailyEssentials.map((x) => (
+          <div
+            className="bg-gray-100 p-4 rounded-[15px] text-center"
+            key={x._id}
+          >
+            <img
+              className="w-[130px] h-[100px] rounded-[15px]"
+              src={x.image}
+              alt="dailEssential"
+            />
+            <h2 className="text-gray-500">{x.title}</h2>
+            <h1 className="font-bold text-[14px]">UP TO 50% OFF</h1>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,53 @@
 import React from "react";
-
+import { BsApple } from "react-icons/bs";
+import { FaGooglePlay } from "react-icons/fa";
+import { BsWhatsapp } from "react-icons/bs";
+import { MdCall } from "react-icons/md";
 function Footer() {
   return (
     <div className="bg-[#008ECC] py-8">
       <div className="w-4/5 m-auto grid sm:grid-cols-1 md:grid-cols-3 gap-4">
         <div className="">
           <h1 className="font-bold text-white">AliExpress</h1>
+          <div>
+          <h2 className=" font-bold text-white py-8">Contact Us</h2>
+          <div className="flex items-center text-white">
+            <div className="mr-4">
+              <BsWhatsapp size={25} />
+            </div>
+            <div className="">
+              <h3>WhatsApp</h3>
+              <p>+212698487054</p>
+            </div>
+          </div>
+          <div className="my-8 flex items-center text-white">
+            <div className="mr-4">
+              <MdCall size={25} />
+            </div>
+            <div className="">
+              <h3>Call Us</h3>
+              <p>+212698487054</p>
+            </div>
+          </div>
+          {/* download app */}
+          <h1 className="text-white font-bold text-xl my-4">Download App</h1>
+          <div className="flex items-center"> 
+          <div className="bg-black p-2 rounded cursor-pointer text-white mr-4 flex items-center">
+              <BsApple size={25} />
+              <div className="ml-2">
+                <p className="text-[10px]">Download on </p>
+                <h1 className="font-bold  text-white">App Store</h1>
+              </div>
+            </div>
+            <div className="bg-black p-2 rounded cursor-pointer text-white   flex items-center">
+              <FaGooglePlay size={25} />
+              <div className="ml-2">
+                <p className="text-[10px]">Get it from</p>{" "}
+                <h1 className="font-bold   text-white">GooglePlay</h1>
+              </div>
+            </div>
+            </div>
+          </div>
         </div>
         <div className="">
           <h2 className="text-white border-b-2 border-white pb-4">
@@ -22,7 +64,7 @@ function Footer() {
             <li className="my-4 text-white">Diary Backery</li>
           </ul>
         </div>
-        <div className="">
+       <div className="">
           <h2 className="text-white border-b-2 pb-4 border-white">
             Customer Services
           </h2>
@@ -34,7 +76,14 @@ function Footer() {
             <li className="my-4 text-white">E-Waste Policy</li>
             <li className="my-4 text-white">Cancellation & Return Policy</li>
           </ul>
-        </div>
+        </div> 
+      </div>
+      {/* copyright */}
+      <div className="w-4/5 m-auto mt-8 text-white text-center">
+        <div className="border-t-[1px] border-[#05ABF3] pb-2" />
+        <p>
+          &copy; {new Date().getFullYear()} All rights reserved.Reliance Ltd
+        </p>
       </div>
     </div>
   );
